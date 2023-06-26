9b9aeccd850c00381e653821f770f912bbf8d025
olamund0 <daniellsoouza380@gmail.com>
2023-06-26 20:36:31 -0300

    SQL

---
+++ mainSQL.sql
@@ -0,0 +1,9 @@
+CREATE TABLE manifestacao (
+    codigo INT AUTO_INCREMENT,
+    titulo VARCHAR(100),
+    descricao VARCHAR(1000),
+    tipo VARCHAR(100),
+    autor VARCHAR(100),
+    data VARCHAR(100),
+    PRIMARY KEY (codigo)
+);
